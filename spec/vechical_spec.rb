d5b853484e2fd0e46a67f03d2edaf5220703574b
Trevor Scherling <scherling.trevor@gmail.com>
2015-08-10 00:21:01 -0700

    Practicing codding

---
+++ spec/vechical_spec.rb
@@ -0,0 +1,19 @@
+# require('rspec')
+# require('vehicle')
+#
+# describe(Vehicle) do
+#   describe("#age") do
+#     it("returns the vehicle's age") do
+#       test_vehicle = Vehicle.new("Ford", "Ranger", 2000)
+#       expect(test_vehicle.age()).to(eq(15))
+#     end
+#   end
+#
+#   describe("#worth_buying?") do
+#     it("returns false if the vehicle is not american made and younger than 16 years old") do
+#       test_vehicle = Vehicle.new("Ford", "Ranger", 2000)
+#       expect(test_vehicle.age()).to(eq(15))
+#
+#     end
+#   end
+# end
